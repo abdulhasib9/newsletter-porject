e238484612f286387b8b48e3a79bd53e09b2d176
abdul hasib <yousufzai.abdulhasib@gmail.com>
2023-05-17 12:00:21 -0700

    finished the sign up page design and submited the data to server

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const bodyParser = require("body-parser")
 const request = require("request")
 const app = express()
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.get("/",(req,res)=>{
    res.sendFile(__dirname+"/index.html")
+})
+
+app.post("/",(req,res)=>{
+    //console.log(req.body)
+    let firstName = req.body.firstNameInput
+    let lastName = req.body.lastNameInput 
+    let email = req.body.emailInput 
+
+    
 })
 
 app.listen(3000,()=>{
